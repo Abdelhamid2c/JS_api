09e7737cd8d912a94b169bfac2ad8de9785a066b
Abdelhamid CHEBEL <abdelhamidchebel25@gmail.com>
2023-07-01 15:15:29 +0200

    .

---
+++ withPromises.js
@@ -0,0 +1,64 @@
+function getAllUsers() {
+    return new Promise(function(resolve, reject) {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then((response) => {
+            if(response.ok)
+                response.json()
+            else
+                reject("error with users request")
+            })
+        .then(users => 
+            {
+                document.getElementById('mini-box').innerHTML = ""
+                for(user of users){
+                    let content = `
+                                    <div id="user" onClick = "userClicked(${user.id},this)">
+                                        <h3>${user.name}</h3>
+                                        <h3>${user.phone}</h3>
+                                    </div>
+                                
+                    `
+                    console.log(user);
+                    document.getElementById('mini-box').innerHTML += content;
+            }
+            resolve()
+        })
+    })
+
+    }
+
+    function getAllPosts(id) {
+         fetch('https://jsonplaceholder.typicode.com/posts?userId='+id)
+        .then((response) => response.json())
+        .then(posts => 
+            {
+                document.getElementById('posts').innerHTML = ""
+                for(post of posts){
+                    let content = `
+                                    <div id="box" style="display:contents;">
+                                        <div id="mini-box">
+                                            <div id="user">
+                                                <h3>${post.title}</h3>
+                                                <h3>${post.body}</h3>
+                                            </div>
+                                        </div>
+                                    </div>
+                    `
+                    console.log(post);
+                    document.getElementById('posts').innerHTML += content;
+            }
+        })
+        }
+
+        // obtenir users aprés les posts
+
+    getAllUsers().then(function(){
+        getAllPosts(1)
+    }).catch(function(e){
+        console.log(e)
+    })
+
+
+
+
+
