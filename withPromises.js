a6e1798181fa517d3a983d852c97f43373090b84
Abdelhamid CHEBEL <abdelhamidchebel25@gmail.com>
2023-07-10 13:45:56 +0200

    .

--- withPromises.js
+++ withPromises.js
@@ -20,7 +20,7 @@ function getAllUsers() {
                     `
                     console.log(user);
                     document.getElementById('mini-box').innerHTML += content;
-            }
+                 }
             resolve()
         })
     })
